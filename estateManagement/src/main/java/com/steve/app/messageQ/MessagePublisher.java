24320a8a0a228dbd373f7ee1ae4ebd874f647b1c
steve <steve199something@hotmail.com>
2021-12-12 14:25:18 +0200

    mq complete

--- estateManagement/src/main/java/com/steve/app/messageQ/MessagePublisher.java
+++ estateManagement/src/main/java/com/steve/app/messageQ/MessagePublisher.java
@@ -5,9 +5,12 @@ import java.util.UUID;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class MessagePublisher {
@@ -15,8 +18,17 @@ public class MessagePublisher {
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
 	
-	@PostMapping("/publish")
-	public String publishMessage(@RequestBody CustomMessage message) {
+	@GetMapping("/publish")
+	public ModelAndView publish() {
+		ModelAndView mav = new ModelAndView("publish/view");
+		CustomMessage message = new CustomMessage();
+		mav.addObject("message", message);
+		return mav;
+	}
+	
+	@PostMapping("/publish/send")
+	public String publishMessage(@ModelAttribute CustomMessage message) {
+		
 		message.setMessageId(UUID.randomUUID().toString());
 		message.setMessageId(LocalDate.now().toString());
 		rabbitTemplate.convertAndSend(MQConfig.EXCHANGE,
